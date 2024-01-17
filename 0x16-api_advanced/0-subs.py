e97fa9e4e98561fba407da2839148685c408e913
mikehailu7 <mikehailu7@gmail.com>
2024-01-16 22:23:12 -0800

    api advanced by mikias hailu

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-# Author: MikiasHailu
-""" This Module is for the number of subscribers """
+"""Module for task 0"""
+
 
 def number_of_subscribers(subreddit):
-	""" This fucntion will queries the Reddit API and returns the number of subscribers """
+	"""Queries the Reddit API and returns the number of subscribers
+		to the subreddit"""
 		import requests
 
 		sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
