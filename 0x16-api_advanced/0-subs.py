534754423d38d69c976d8618fc58bf41d13acfc2
mikehailu7 <mikehailu7@gmail.com>
2024-01-16 22:33:12 -0800

    api advanced by mikias hailu

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0
-"""
-
+""" Exporting csv files"""
+import json
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-	"""
-		Function that queries the Reddit API
-		- If not a valid subreddit, return 0.
-		"""
-		req = requests.get(
-				"https://www.reddit.com/r/{}/about.json".format(subreddit),
-				headers={"User-Agent": "Custom"},
-				)
-
-		if req.status_code == 200:
-		return req.json().get("data").get("subscribers")
-		else:
-		return 0
+	"""Read reddit API and return number subscribers """
+		username = 'ledbag123'
+		password = 'Reddit72'
+		user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+		headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+		url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+client = requests.session()
+	client.headers = headers
+r = client.get(url, allow_redirects=False)
+	if r.status_code == 200:
+	return (r.json()["data"]["subscribers"])
+	else:
+return(0)
